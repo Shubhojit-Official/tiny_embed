0e5b49b133f054ccd58f529cbee6a5d7e6d014fa
Shubhojit Ghosh <shubhojitghosh.005@gmail.com>
2026-09-19 14:41:37 +0530

    Training the model on entire corpus for 100 epochs

--- app.py
+++ app.py
@@ -30,6 +30,40 @@ def softmax(z):
     exp_z = np.exp(z - np.max(z))
     return exp_z / np.sum(exp_z)
 
+def train_step(input_id, target_id, W_in, W_out, learning_rate):
+    # -------Forward Pass-----------
+
+    # Get Embedding for the input
+    h = W_in[input_id]
+
+    # Calculate Logits
+    z = h @ W_out
+    
+    # Softmax
+    p = softmax(z)
+
+    # Loss function (Cross-Entropy)
+    loss = -np.log(p[target_id])
+
+    # ------Back Propagation--------
+
+    # Gradient of loss wrt logits (dL/dz)
+    dz = p.copy()
+    dz[target_id] -= 1
+
+    # Gradient for output matrix  (dL/dWout)
+    dWout =  np.outer(h, dz) # (h{trans} * p - y )
+
+    # Gradient for the Embedding (dL/dh)
+    dh = dz @ W_out.T
+
+    #---------Updation----------
+    W_out -= learning_rate * dWout
+    W_in[input_id] -= learning_rate * dh
+
+    return loss
+
+
 if __name__ == "__main__":
     text = "the cat sat on the mat"
     words  = text.split()
@@ -48,38 +82,41 @@ if __name__ == "__main__":
 
     corpus = [word_to_id[word] for word in words]
 
-    X, y = create_training_data(corpus, window_size=1)
 
-    embed_mat = embedding(vocab_size, embedding_dim)
+    np.random.seed(42)
 
-    W_in = embed_mat * 0.01 # Input Embedding Matrix
+    W_in = np.random.randn(vocab_size, embedding_dim) * 0.01
     W_out = np.random.randn(embedding_dim, vocab_size) * 0.01
 
+    X, y = create_training_data(corpus,1)
 
-    input_id = word_to_id['cat']
-    target_id = word_to_id['sat']
+    epochs = 100
+    for epoch in range(epochs):
+        total_loss = 0
+        for input_id, target_id in zip(X, y):
+            loss = train_step(
+                input_id,
+                target_id,
+                W_in,
+                W_out,
+                learning_rate=0.1
+            )
+            total_loss += loss
 
-    # Get Embedding for the input
-    h = W_in[input_id]
+        average_loss = total_loss / len(X)
+        if epoch % 10 == 0:
+            print(
+            f"Epoch {epoch}, "
+            f"Loss: {average_loss:.4f}"
+            )
 
-    # Calculate Logits
-    z = h @ W_out
+    for word in vocab:
+        word_id = word_to_id[word]
 
-    # Softmax
-    p = softmax(z)
+        print(
+            word,
+            W_in[word_id]
+        )
 
-    # Loss function (Cross-Entropy)
-    loss = -np.log(p[target_id])
-
-    # Gradient of loss wrt logits
-    dz = p.copy()
-    dz[target_id] -= 1
-
-    # Gradient for output matrix (dL/dW_out = h(transpose)*(p-y))
-    dW_out =  np.outer(h,dz)
-
-    # Gradient for embedding
-    dh = dz @ W_out.T
-    
   
     
